539edf4e1fca095615aa5a4b7916a0533fd5e39f
mexus <spikemorow@gmail.com>
2024-04-08 21:29:20 +0300

    module5-task2 done

--- src/components/offers-list.tsx
+++ src/components/offers-list.tsx
@@ -3,11 +3,12 @@ import Card from './card';
 
 type OffersListProps = {
   offers: OfferType[];
+  listType: 'default' | 'near';
 };
 
-export default function OffersList({offers}: OffersListProps) {
+export default function OffersList({offers, listType}: OffersListProps) {
   return (
-    <div className="cities__places-list places__list tabs__content">
+    <div className={`${listType === 'default' ? 'cities__places-list places__list tabs__content' : 'near-places__list places__list'}`}>
       {offers.map((offer) => (
         <Card key={offer.id} offer={offer}/>
       ))}
