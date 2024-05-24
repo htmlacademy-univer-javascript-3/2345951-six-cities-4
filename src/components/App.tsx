0b3eede7d476927628fafa36924af5fb43f61b23
mexus <spikemorow@gmail.com>
2024-05-24 23:24:03 +0300

    module7-task1 done

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,29 +1,41 @@
 import Main from '../pages/main.tsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NotFoundPage from '../error/NotFound.tsx';
 import Offer from '../pages/offer.tsx';
 import Login from '../pages/login.tsx';
 import Favourites from '../pages/Favourites.tsx';
 import PrivateRoute from '../private/private-route.tsx';
-import {OfferType} from '../types/offer';
+import {useAppSelector} from '../hooks';
+import LoadingScreen from '../pages/Loading-Screen.tsx';
+import NotFoundPage from '../error/NotFound.tsx';
 
-type AppProps = {
-  offers: OfferType[];
-}
-
-export default function App ({offers}: AppProps){
-  const favorites = offers.filter((o) => o.isFavorite);
+function App(): JSX.Element {
+  const isOffersDataLoading = useAppSelector((state) => state.isOffersDataLoading);
+  const offers = useAppSelector((state) => state.offers);
+  const cityOffers = useAppSelector((state) => state.cityOffers);
+  if (isOffersDataLoading) {
+    return (
+      <LoadingScreen />
+    );
+  }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/favorites" element={<PrivateRoute><Favourites favorites={favorites}/></PrivateRoute>} />
-        <Route path="offer/">
-          <Route path=":id" element={<Offer offers={offers}/>} />
+        <Route path="/">
+          <Route index element={<Main />} />
+          <Route path="login" element={<Login />} />
+          <Route path="favorites" element={
+            <PrivateRoute>
+              <Favourites favorites={offers} />
+            </PrivateRoute>
+          }
+          />
+          <Route path="offer/">
+            <Route path=":id" element={<Offer offers={ cityOffers } />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
-        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
+export default App;
