fc8b63da67e6141bf2fad359d1927ec3e12b73fa
mexus <spikemorow@gmail.com>
2024-05-24 23:25:50 +0300

    minor fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import 'leaflet/dist/leaflet.css';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import {offers} from './mocks/offers.ts';
 import {Provider} from 'react-redux';
 import {store} from './store';
 import {fetchOffersAction} from './api/api-cation.ts';
@@ -16,7 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store = { store } >
-      <App offers={ offers }/>
+      <App />
     </Provider>
   </React.StrictMode>
 );
